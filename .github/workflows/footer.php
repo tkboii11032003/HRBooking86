f4002d666afc6e53b57faaa17e2593fcc6df9ebd
tkboii11032003 <teerapatnoimuengpon@gmail.com>
2025-03-02 18:42:26 +0700

    Delete .github/workflows directory

--- .github/workflows/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer py-4">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-start"> &copy;  86FILMS.TH TEAM 2025</div>
-                    <div class="col-lg-4 my-3 my-lg-0">
-                    </div>
-                    <div class="col-lg-4 text-lg-end">
-                    </div>
-                </div>
